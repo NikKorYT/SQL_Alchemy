a423765fe68708e1d1eaa4649314fbc3109ccaf4
Nik Kor <3357381Nk@gmail.com>
2024-08-11 16:17:40 +0100

    base creation finished

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
-#Import necessary modules
+# Import necessary modules
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DATETIME
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-#reate an engine
+# reate an engine
 engine = create_engine("sqlite:///groups.db", echo=True)
 
-#class for creating a table
+# Create a session
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+# class for creating a table
 Base = declarative_base()
 
 class Tutors(Base):
     __tablename__ = "Tutors"
     tutor_id = Column(Integer, primary_key=True)
     name = Column(String)
-    
+
 class Groups(Base):
     __tablename__ = "Groups"
     group_id = Column(Integer, primary_key=True)
     group_name = Column(String)
-    
+
 class Students(Base):
     __tablename__ = "Students"
     student_id = Column(Integer, primary_key=True)
     student_name = Column(String)
     group_id = Column(Integer, ForeignKey("Groups.group_id"))
-    
+
 class Subjects(Base):
     __tablename__ = "Subjects"
     subject_id = Column(Integer, primary_key=True)
@@ -38,3 +42,8 @@ class Marks(Base):
     subject_id = Column(Integer, ForeignKey("Subjects.subject_id"))
     mark = Column(Integer)
     time = Column(DATETIME)
+
+# Create the tables
+Base.metadata.create_all(engine)
+Base.metadata.bind = engine
+
