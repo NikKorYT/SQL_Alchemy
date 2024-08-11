6c61bf23b1cffdf6ad2c78c5f8e683d858c86dbd
Nik Kor <3357381Nk@gmail.com>
2024-08-11 16:12:37 +0100

    Base created

---
+++ main.py
@@ -0,0 +1,40 @@
+#Import necessary modules
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DATETIME
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+#reate an engine
+engine = create_engine("sqlite:///groups.db", echo=True)
+
+#class for creating a table
+Base = declarative_base()
+
+class Tutors(Base):
+    __tablename__ = "Tutors"
+    tutor_id = Column(Integer, primary_key=True)
+    name = Column(String)
+    
+class Groups(Base):
+    __tablename__ = "Groups"
+    group_id = Column(Integer, primary_key=True)
+    group_name = Column(String)
+    
+class Students(Base):
+    __tablename__ = "Students"
+    student_id = Column(Integer, primary_key=True)
+    student_name = Column(String)
+    group_id = Column(Integer, ForeignKey("Groups.group_id"))
+    
+class Subjects(Base):
+    __tablename__ = "Subjects"
+    subject_id = Column(Integer, primary_key=True)
+    subject_name = Column(String)
+    tutor_id = Column(Integer, ForeignKey("Tutors.tutor_id"))
+
+class Marks(Base):
+    __tablename__ = "Marks"
+    mark_id = Column(Integer, primary_key=True)
+    student_id = Column(Integer, ForeignKey("Students.student_id"))
+    subject_id = Column(Integer, ForeignKey("Subjects.subject_id"))
+    mark = Column(Integer)
+    time = Column(DATETIME)
